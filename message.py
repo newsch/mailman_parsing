823ae23a498bf3ef47f19aac5dfb1b1bd8019437
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2017-03-23 16:39:35 -0400

    Add preliminary parent/child relationship

--- message.py
+++ message.py
@@ -1,20 +1,23 @@
 #! /usr/bin/env python3
-# from datetime import datetime
-
-
 class Message:
     def __init__(self,
                  subject,
                  message_id,
                  send_datetime,
                  content,
-                 raw_message):
+                 raw_message,
+                 parent=None,
+                 children=[]):
         self.subject = subject
         self.message_id = message_id
         self.send_datetime = send_datetime
         self.content = content
         self.raw_message = raw_message
-        self.children = []
+        self.parent = parent
+        self.children = children
+
+    def add_parent(self, obj):
+        self.parent = obj
 
     def add_child(self, obj):
         self.children.append(obj)
@@ -27,3 +30,11 @@ class Message:
                 ).format(**{'formatted_datetime':
                             self.send_datetime.strftime('%Y-%m-%d %H:%M:%S'),
                             **self.__dict__})
+
+    def get_thread(self):
+        # thread_str = self.__str__()
+        print(self)
+        for child in self.children:
+            print('^')
+            print(child.get_thread())
+        # return thread_str
--- txt_archive_scraper.py
+++ txt_archive_scraper.py
@@ -4,9 +4,8 @@ import re
 from message import Message
 
 
-def get_raw_messages():
+def get_raw_messages(archive_path='sample_thread_text.txt'):
     '''Separates a txt.gz archive into a list of raw messages'''
-    archive_path = '/home/lloyd/Documents/github/mailing_database/archives/therapy/2017-March.txt.gz'
     with open(archive_path, 'rb') as f:
         file_content = f.readlines()
         # split file into messages
@@ -54,24 +53,34 @@ def process_raw_message(raw_message, remove_quoted_emails=True):
         elif key == 'content' and remove_quoted_emails:
             quote_stripped_group = pattern_group
             quote_patterns = {
-                'Original Message': '^-+Original Message-+\n',
-                'On_,_wrote': '^On [^,\n]*, .*\n{0,1}.* wrote:\n',
+                'Original Message': '^-+ *Original Message *-+\n',
+                '  >> Original Message': '^    >> \n    >> -+Original Message-+\n',
+                'On_,_wrote': '^On [^,\n]*, .*\n{0,1}.*wrote:\n',
                 '> On_,_wrote': '^> On [^,\n]*, .*\n{0,1}.* wrote:\n',
                 'From_Sent_Subject_To_': '^From: (.*)\nSent: (.*)\nSubject: (.*)\nTo: (.*)\n',
                 '_': '^_+\n',
+                # '>': '^\s*>+\s*.*\n'
             }
             for quote_key, quote_pattern in quote_patterns.items():
-                quote_pattern_match = re.search(quote_pattern, pattern_group, re.MULTILINE)
+                quote_pattern_match = re.search(quote_pattern, pattern_group, re.MULTILINE | re.IGNORECASE)
                 if quote_pattern_match:
                     print('Found "{}" quote pattern.'.format(quote_key))
                     print(quote_pattern_match.group())
-                    quote_stripped_group = pattern_group[:quote_pattern_match.start()]
+                    quote_stripped_group = quote_stripped_group[:quote_pattern_match.start()]
             message_values[key] = quote_stripped_group.strip('\n')
         elif key == 'content':
             message_values[key] = pattern_group.strip('\n')
         else:
             message_values[key] = pattern_group
     # save to message object
+    # check values
+    content_check_patterns = {
+        'is only whitespace': re.compile('\s+'),
+        'contains >': re.compile('[\s\S]*^\s*>+[\s\S]*'),
+    }
+    for content_check_type, content_check_pattern in content_check_patterns.items():
+        if content_check_pattern.match(message_values['content']):
+            print('warning: message {} content {}').format(message_values['message_id'], content_check_type)
     return Message(**message_values)
 
 
@@ -80,53 +89,37 @@ def process_raw_messages(raw_messages):
     return [process_raw_message(msg) for msg in raw_messages]
 
 
-test_msg = '''
-From therapy at lists.olin.edu  Wed Mar  1 14:24:41 2017
-From: therapy at lists.olin.edu (Anonymous rants and raves.)
-Date: Wed, 1 Mar 2017 19:24:41 +0000
-Subject: [Therapy] Mistake
-In-Reply-To: <mailman.219.1488396127.1623.therapy@lists.olin.edu>
-References: <mailman.214.1488394958.1623.therapy@lists.olin.edu>
-	<mailman.218.1488395986.1623.therapy@lists.olin.edu>
-	<mailman.219.1488396127.1623.therapy@lists.olin.edu>
-Message-ID: <mailman.220.1488396294.1623.therapy@lists.olin.edu>
+def link_messages(messages):
+    '''set parents and children of messages for a list of messages'''
+    # set parents based on In-Reply-To: field
+    reply_to_pattern = re.compile('^In-Reply-To: (.*)\n', re.MULTILINE)
+    for message in messages:
+        reply_to_match = reply_to_pattern.search(message.raw_message)
+        if reply_to_match:
+            reply_id = reply_to_match.group(1)
+            if reply_id != message.message_id:
+                for potential_parent in messages:
+                    if potential_parent.message_id == reply_id:
+                        message.add_parent(potential_parent)
+                        print('Found parent {} for message {}'.format(potential_parent.message_id, message.message_id))
+                        potential_parent.add_child(message)
+        else:
+            print('No "In-Reply-To" field found for message ' + message.message_id)
+    print('Finished finding parents')
+    # set children based on new parents
+    # for message in messages:
+    #     if message.parent and message not in message.parent.children:
+    #         message.parent.add_child(message)
+    #         print('Added child {} to message {}'.format(message.message_id, message.parent.message_id))
 
-Yo knobheads, this conversation is only going to get dangerously close to
-naming names. Stop.
 
-On Wed, Mar 1, 2017 at 2:22 PM Anonymous rants and raves. <
-therapy at lists.olin.edu> wrote:
-
-> Ooh, lets guess! Okay, going off of subtext, I'm assuming yuuuge is a
-> Trump reference. So did the mistake person get into a political argument
-> with OP in which they were very much pro Trump? Thoughts?
-> -----Original Message-----
-> From: Therapy [mailto:therapy-bounces at lists.olin.edu] On Behalf Of
-> Anonymous rants and raves.
-> Sent: Wednesday, March 1, 2017 2:20 PM
-> To: Anonymous rants and raves. <therapy at lists.olin.edu>
-> Subject: Re: [Therapy] Mistake
->
-> Dear Therapy,
->
-> I want to write a nasty anonymous email complaining about a single person.
-> I can't name names on Therapy, because it is against the rules. Because I
-> am a little bitch, my solution is to write my nasty email anyways, and
-> instead of actually naming the person in question, just give vague hints.
-> This will satisfy my desperate need to whine, while also inspiring
-> everyone else to try and guess who I meant.
->
-> Sincerely,
-> Anonymous Douchecanoe
->
-> On Wed, Mar 1, 2017 at 2:02 PM Anonymous rants and raves. <
-> therapy at lists.olin.edu> wrote:
->
-> > One of the class of 2017 was a yuuuge mistake by Olin admissions
-> >
->
-'''
-print(process_raw_message(test_msg))
 # test = get_raw_messages()
-# print(process_raw_message(test[0]),)
+# # print(process_raw_message(test[0]),)
 # print(*process_raw_messages(test), sep='\n')
+
+test_messages = process_raw_messages(get_raw_messages(archive_path='sample_thread_text_2.txt'))
+link_messages(test_messages)
+# test_message = test_messages[1]
+for test_message in test_messages:
+    print('MESSAGE: {}PARENT: {}CHILDREN:\n{}'.format(test_message, test_message.parent, (*test_message.children)))
+# test_messages[8].get_thread()
